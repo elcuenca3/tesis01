6b8b38f9696691c6eb7f3b9c350aa6e89fb1507c
ErickCu <erick.cuenca.cl@gmail.com>
2023-07-13 12:16:24 -0500

    prueba para deploy rail

--- Aplicativo/settings.py
+++ Aplicativo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '^adwke-#3oll)1@5wu-%p=d4%i_6vuo+hg@8(kgvoyubpn(*jc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.netlify.app']
+ALLOWED_HOSTS = ["*"]
 # 'brandonvs.pythonanywhere.com', 'localhost', '127.0.0.1',
 
 # Application definition
