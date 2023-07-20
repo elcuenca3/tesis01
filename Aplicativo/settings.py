dc7a38d6f64b127bd48c59f3cc96ef92c7158b5a
ErickCu <erick.cuenca.cl@gmail.com>
2023-07-20 10:15:42 -0500

    funciono

--- Aplicativo/settings.py
+++ Aplicativo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '^adwke-#3oll)1@5wu-%p=d4%i_6vuo+hg@8(kgvoyubpn(*jc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -133,8 +133,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
